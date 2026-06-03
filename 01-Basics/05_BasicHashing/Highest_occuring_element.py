b0f3569c9d778c1d9f7b9700929d548bac8b8995
lahari-prasanna <200882349+lahari-prasanna@users.noreply.github.com>
2026-06-03 22:26:33 +0530

    Implement Basic Hashing problems

---
+++ 01-Basics/05_BasicHashing/Highest_occuring_element.py
@@ -0,0 +1,16 @@
+# Given an array nums of n integers, find the most frequent element in it i.e., the element that occurs the maximum number of times. If there are multiple elements that appear a maximum number of times, find the smallest of them.
+
+def highest_occuring_element(arr):
+    freq={}
+    for num in arr:
+        freq[num]=freq.get(num,0)+1
+    max_freq=max(freq.values())
+    result=[]
+    for key,value in freq.items():
+        if value==max_freq:
+            result.append(key)
+    return min(result)
+
+arr=[1,1,1,2,2,2,3,4,]
+print(highest_occuring_element(arr))
+
---
+++ 01-Basics/05_BasicHashing/count_frequency.py
@@ -0,0 +1,16 @@
+# Given an array nums of size n which may contain duplicate elements.
+# Rreturn a list of pairs where each pair contains a unique element from the array and its frequency in the array.
+# You may return the result in any order, but each element must appear exactly once in the output.
+
+def count_frequency(arr):
+    freq={}
+    for num in arr:
+        freq[num]=freq.get(num,0)+1
+    result=[]
+    print(freq.items())
+    for key,value in freq.items():
+        result.append([key,value])
+    return result
+
+arr=[1,2,2,1,3]
+print(count_frequency(arr))
