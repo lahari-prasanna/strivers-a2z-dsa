b78b525e8d7f77416d5879e66b1b61bee338b477
lahari-prasanna <200882349+lahari-prasanna@users.noreply.github.com>
2026-06-20 08:50:03 +0530

    Update Bubble sort algorithm

--- 02-SortingTechniques/bubble_sort.py
+++ 02-SortingTechniques/bubble_sort.py
@@ -5,16 +5,23 @@
 def bubble_sort(nums):
     n=len(nums)
     for i in range(0,n-1):
+        swap=0
         for j in range(0,n-i-1):
             if nums[j]>nums[j+1]:
                 #swap
                 temp=nums[j]
                 nums[j]=nums[j+1]
                 nums[j+1]=temp
+                swap=1
+        print("count")
+        if( not swap):
+            return nums
     return nums
 
 
 
-nums=[2,50,15,64,36]
+nums=[4,5,6,1,3,2]
 result=bubble_sort(nums)
 print(result)
+
+#TC: worst and avg: O(n^2)  best: O(n)
